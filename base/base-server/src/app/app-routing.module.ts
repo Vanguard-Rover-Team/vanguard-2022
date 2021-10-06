b940c680c9d7ff9b7f84b0902a189a1d758660a9
bdpyle02 <mail4brando@gmail.com>
2021-10-05 22:19:09 -0400

    Created the navigation bar

--- base/base-server/src/app/app-routing.module.ts
+++ base/base-server/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArmComponent } from './arm/arm.component';
+import { AutoComponent } from './auto/auto.component';
+import { CameraComponent } from './camera/camera.component';
+import { MainComponent } from './main/main.component';
+import { TunerComponent } from './tuner/tuner.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', component: MainComponent},
+  { path: 'camera', component: CameraComponent },
+  { path: 'tuner', component: TunerComponent },
+  { path: 'arm', component: ArmComponent },
+  { path: 'auto', component: AutoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- base/base-server/src/app/app.module.ts
+++ base/base-server/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CameraComponent } from './camera/camera.component';
+import { TunerComponent } from './tuner/tuner.component';
+import { ArmComponent } from './arm/arm.component';
+import { AutoComponent } from './auto/auto.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MainComponent,
+    CameraComponent,
+    TunerComponent,
+    ArmComponent,
+    AutoComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
